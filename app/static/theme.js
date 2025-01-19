32d44242ef223dc93caf1e9a1875f2b05a1714f7
SherifCaptainRed <anshtrips07@gmail.com>
2025-01-19 21:30:04 +0530

    completing styling

---
+++ app/static/theme.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("theme-toggle");
+  const body = document.body;
+
+  toggleButton.addEventListener("click", function () {
+    body.classList.toggle("dark-theme");
+  });
+});
